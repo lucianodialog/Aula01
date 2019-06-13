42c5d5ca6768a461a2575de14fb324231bb893a6
Luciano Silva <luciano_dialog@yahoo.com.br>
2019-06-13 13:35:56 -0300

    alterada a mensagem

--- src/Aula01_Git.java
+++ src/Aula01_Git.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Aula01_Git {
 	
@@ -9,6 +11,14 @@ public class Aula01_Git {
 		int idade = 39;
 		
 		System.out.println("Aula 01 - Git");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		nome = sc.nextLine();
+		
+		System.out.println(nome);
+		
 		
 		
 

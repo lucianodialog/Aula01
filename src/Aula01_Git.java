c62fb1c4a84b14cf6e4165a8b185320f6a8013df
Luciano Silva <luciano_dialog@yahoo.com.br>
2019-06-13 12:57:44 -0300

    Mensagem da Aula01

--- src/Aula01_Git.java
+++ src/Aula01_Git.java
@@ -1,12 +1,11 @@
 
 public class Aula01_Git {
-
-	public Aula01_Git() {
-		// TODO Auto-generated constructor stub
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Aula 01 - Git");
 
 	}
 

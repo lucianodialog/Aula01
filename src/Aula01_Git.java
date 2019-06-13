7c16a7ea6cbb9647be3f08b78433a7a37facd7b9
Luciano Silva <luciano_dialog@yahoo.com.br>
2019-06-13 13:06:27 -0300

    Adicionado Variaveis nome e idade

--- src/Aula01_Git.java
+++ src/Aula01_Git.java
@@ -5,7 +5,12 @@ public class Aula01_Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String nome = "Luciano";
+		int idade = 39;
+		
 		System.out.println("Aula 01 - Git");
+		
+		
 
 	}
 
